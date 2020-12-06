be0171dd10b36580b43d000746f96a53dc38157e
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 20:16:45 +0100

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -29,7 +29,7 @@ class Course{
     return lecturer;
   }
   public void setCredit(int newCaredit){
-    this.credit = newCredit
+    this.credit = newCredit;
   }
   public int getCredit(){
     return credit;
