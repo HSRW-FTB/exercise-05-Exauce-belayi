07d21f97fb190a8dee169f88553df9f77f32a529
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 22:07:32 +0100

    Update TestCourse.java

--- src/main/java/TestCourse.java
+++ src/main/java/TestCourse.java
@@ -1,6 +1,6 @@
 public class TestCourse{
   public static void main(String args[] ){
-  Course course1 = new Course();
+  Course course1 = new Course(String, String, Professor, int);
 
   System.out.println("The course name is ," + course1.getName() + "The lecturer is ," + course1.getLecturer());
   System.out.println("The course id is ," + course1.getID() + "The course credit is ," + course1.getCredit());
