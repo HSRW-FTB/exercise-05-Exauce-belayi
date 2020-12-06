468e12d9d982da3d00bd3d2628c85f655fae2f89
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 21:33:53 +0100

    Create Classroom.java

---
+++ src/main/java/Classroom.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Classroom {
+    private String id;
+    private Course course;
+    private ArrayList<Student> participants = new ArrayList<Student>();
+    private String room;
+    private String term;
+
+    private Classroom(){}
+
+    public Classroom(Course course, String room){
+        this.course = course;
+        this.room = room;
+        Date date = new Date();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        //Add one to month {0 - 11}
+        int month = calendar.get(Calendar.MONTH) + 1;
+
+        if (month<10 & month >2){
+            term = "SS"+year+"/"+((year+1)%100);
+        }else{
+            term = "WS"+year+"/"+((year+1)%100);
+        }
+
+        this.id=course.getID()+"-"+ term;
+    }
+    public Classroom(Course course, String room, Date date){
+        this.course = course;
+        this.room = room;
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        //Add one to month {0 - 11}
+        int month = calendar.get(Calendar.MONTH) + 1;
+
+        if (month<10 & month >2){
+            term = "SS"+year+"/"+((year+1)%100);
+        }else{
+            term = "WS"+year+"/"+((year+1)%100);
+        }
+
+        this.id=course.getID()+"-"+ term;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public Course getCourse() {
+        return this.course;
+    }
+
+    public String getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getTerm() {
+        return this.term;
+    }
+
+    public void addStudent(Student student){
+        participants.add(student);
+    }
+
+    public String getParticipantsEmail(){
+        String emails = "";
+        for (int i=0; i < participants.size();i++){
+            emails = emails + participants.get(i).getFQUN() + ", ";
+        }
+        return emails.substring(0, emails.length() - 2);
+    }
+    public int getTotalParticipants(){
+        return participants.size();
+    }
+
+}
