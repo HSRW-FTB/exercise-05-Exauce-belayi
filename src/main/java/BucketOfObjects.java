1e080cd2104d5ca804c9052d08131869387bbb43
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 21:40:05 +0100

    Update BucketOfObjects.java

--- src/main/java/BucketOfObjects.java
+++ src/main/java/BucketOfObjects.java
@@ -1,32 +1,32 @@
 import java.util.ArrayList;
 
-public class BucketOfObjects{
-  private ArrayList<GeometricObject> items = new ArrayList<GeometricObject>();
-  BucketOfObjects(){
-  }
-  public void addObject(GeometricObject){
-    items.add(object);
-  }
-  public double getTotalArea(){
-    double totalArea = 0.0;
-    for (int i = 0; i < items.size(); i++){
-      totalArea += items.get(i).getArea();
+public class BucketOfObjects {
+    private ArrayList<GeometricObject> items = new ArrayList<GeometricObject>();
+
+    public BucketOfObjects(){}
+
+    public void addObject(GeometricObject object){
+        items.add(object);
     }
-    return totalArea;
-  }
-  public double getTotalPerimeter(){
+    public double getTotalArea(){
+        double totalArea = 0.0;
+        for (int i = 0; i < items.size();i++){
+            totalArea += items.get(i).getArea();
+        }
+        return totalArea;
+    }
+    public double getTotalPerimeter(){
         double totalPerimeter = 0.0;
-    for (int i = 0; i < items.size(); i++){
-      totalPerimeter += items.get(i).getPerimeter();
+        for (int i = 0; i < items.size();i++){
+            totalPerimeter += items.get(i).getPerimeter();
+        }
+        return totalPerimeter;
     }
-    return totalPerimeter;
-  }
-  public String toString(){
-    String str = "";
-    for (int i = 0; i < items.size(); i++){
-      str = str + items.get(i).getClass().getSimpleName() + ",";
+    public String toString(){
+        String str = "";
+        for (int i = 0; i < items.size();i++){
+            str = str + items.get(i).getClass().getSimpleName() + ", ";
+        }
+        return str.substring(0, str.length() - 2);
     }
-    return str.substring(0, str.length() - 2);
-  }
-  
 }
