17cc853bea63a93da26822faeba992fd4496eaf5
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 20:24:03 +0100

    Update BucketOfObjects.java

--- src/main/java/BucketOfObjects.java
+++ src/main/java/BucketOfObjects.java
@@ -3,12 +3,16 @@ class BucketOfObjects{
   BucketOfObjects(){
   }
   public void addObject(GeometricObject){
+    items = GeometricObject;
   }
   public double getTotalArea(){
+    return getArea();
   }
   public double getTotalPerimeter(){
+    return getPerimeter();
   }
   public String toString(){
+    return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
   }
   
 }
