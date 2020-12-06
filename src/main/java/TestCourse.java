ecb2a949492723fa41ba80a4701d1544b338621f
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 21:47:09 +0100

    Delete TestCourse.java

--- src/main/java/TestCourse.java
+++
@@ -1,8 +0,0 @@
-public class TestCourse{
-  public static void main(String args[] ){
-  Course course1 = new Course();
-  
-  System.out.println("The course name is ," + course1.getName() + "The lecturer is ," + course1.getLecturer());
-  System.out.println("The course id is ," + course1.getID() + "The course credit is ," + course1.getCredit());
-  }
-}
