b5bc56abb795efcf847cadbd5e417efbca9a616b
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 21:45:18 +0100

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,38 +1,46 @@
-class Course{
-  private String id;
-  private String name;
-  private Professor lecturer;
-  private int credit;
-  
-  private Course(){
-  }
-  
-  Course(String id, String name, Professor lecturer, int credit){
-    this.id = id;
-    this.name = name;
-    this.lecturer = lecturer;
-    this.credit = credit;
-  }
-  public String getID(){
-    return id;
-  }
-  public void setName(String newName){
-    this.name = newName;
-  }
-  public String getName(){
-    return this.name;
-  }
-  public void setLecturer(Professor newLecturer){
-    this.lecturer = newLecturer;
-  }
-  public Professor getLecturer(){
-    return lecturer;
-  }
-  public void setCredit(int newCaredit){
-    this.credit = newCredit;
-  }
-  public int getCredit(){
-    return credit;
-  }
-  
+public class Course {
+    private String id;
+    private String name;
+    private Professor lecturer;
+    private int credit;
+    
+    private Course(){
+
+    }
+    public Course(String id, String name, Professor lecturer, int credit){
+        this.id = id;
+        this.name = name;
+        this.lecturer = lecturer;
+        this.credit = credit;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Professor getLecturer() {
+        return this.lecturer;
+    }
+
+    public void setLecturer(Professor lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public int getCredit() {
+        return this.credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
 }
+
