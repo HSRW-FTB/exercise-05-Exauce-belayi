4514ed03cb31d973e301573bd3ffb50255c2b6a2
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 22:17:25 +0100

    Update TestCourse.java

--- src/main/java/TestCourse.java
+++ src/main/java/TestCourse.java
@@ -4,7 +4,7 @@ public class TestCourse{
     Professor prof1  = new Professor("xyq", "X", "Yq");
     Course oop = new Course("EL-2309", "Object Oriented Programming", prof1, 5);
 
-  System.out.println("The course name is ," + course1.getName() + "The lecturer is ," + course1.getLecturer());
-  System.out.println("The course id is ," + course1.getID() + "The course credit is ," + course1.getCredit());
+  System.out.println("The course name is ," + getName() + "The lecturer is ," + getLecturer());
+  System.out.println("The course id is ," + getID() + "The course credit is ," + getCredit());
   }
 }
