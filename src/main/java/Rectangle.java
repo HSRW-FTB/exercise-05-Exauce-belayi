75d42387b6a5cd1bd3ad22ad6c2b3c6349d8e607
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 20:14:11 +0100

    Update Rectangle.java

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -23,4 +23,40 @@ else super.setFilled(false);
 void changeColor(String color) {
 super.setColor(color);
 }
+  
+	public double getWidth() {
+		return width;
+	}
+
+	
+	public void setWidth(double width) {
+		this. width = width;
+	}
+
+
+	public double getheight() {
+		return height;
+	}
+
+	
+	public void setheight(double height) {
+		this.height = height;
+	}
+
+	
+	public double getArea() {
+		return width * height;
+	}
+
+	
+	public double getPerimeter() {
+		return 2 * (width * height);
+	}
+
+
+	public String toString() {
+		return super.toString() + " Rectangle, Created: " + getDateCreated() + ", Width: " + width + ", Height: " + height;
+	}
 }
+
+
