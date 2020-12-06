430bef2133f7b546c7d8e2e79a463bac58e4151c
Exauce-belayi <64260200+Exauce-belayi@users.noreply.github.com>
2020-12-06 17:41:40 +0100

    Update Course_class.java

--- src/main/java/Course_class.java
+++ src/main/java/Course_class.java
@@ -1,1 +1,38 @@
-
+class Course_class{
+  private String id;
+  private String name;
+  private Professor lecturer;
+  private int credit;
+  
+  private Course(){
+  }
+  
+  Course(String id, String name, Professor lecturer, int credit){
+    this.id = id;
+    this.name = name;
+    this.lecturer = lecturer;
+    this.credit = credit;
+  }
+  public String getID(){
+    return id;
+  }
+  public void setName(String newName){
+    this.name = newName;
+  }
+  public String getName(){
+    return this.name;
+  }
+  public void setLecturer(Professor newLecturer){
+    this.lecturer = newLecturer;
+  }
+  public Professor getLecturer(){
+    return lecturer;
+  }
+  public void setCredit(int newCaredit){
+    this.credit = newCredit
+  }
+  public int getCredit(){
+    return credit;
+  }
+  
+}
